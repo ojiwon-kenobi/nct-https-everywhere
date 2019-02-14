c6bd0f20d5f84087fd4a292e9337f8da4e6a6e8d
Nathan Malkin <nmalkin@cs.berkeley.edu>
2019-02-14 15:15:12 -0800

    Show a few different ways to check for extension

--- core/nudgechecktrack.js
+++ core/nudgechecktrack.js
@@ -1,119 +1,69 @@
+const httpsEverywhereID = "https-everywhere@eff.org";
 
-
-var httpsEverywhereID = "https-everywhere@eff.org";
-var userID= localStorage.getItem('hostID')/ 4;
-var currentDate = new Date();
-
-function MakeQuerablePromise(promise) {
-    if (promise.isResolved)
-        return promise;
-    var isPending = true;
-    var isRejected = false;
-    var isFulfilled = false;
-
-    var result = promise.then(function (v) {
-        isFulfilled = true;
-        isPending = false;
-        return v;
-    }, function (e) {
-        isRejected = true;
-        isPending = false;
-        catch(e);
-    });
-
-    result.isFulfilled = function () {
-        return isFulfilled;
-    };
-    result.isPending = function () {
-        return isPending;
-    };
-    result.isRejected = function () {
-        return isRejected;
-    };
-    return result;
-
-}
-
-function resetAlarm (userID) {
-    browser.alarms.clearAll()
-    browser.alarms.create({periodInMinutes: localStorage.getItem('period')[userID]})
-}
-//track = updateSettings
-function updateSettings () {
-    localStorage.setItem('nudgeRepeat', localStorage.getItem('nudgeRepeat') + 1)
-    var ke = [localStorage.getItem('times')]
-    console.log(ke)
-    localStorage.setItem('times', ke.push(Date.now()))
-}
-
-function nudge(userID) {
-    browser.notifications.create({
-        type: 'basic',
-        title: 'HTTPS_everywhere checker',
-        message: localStorage.getItem('messages')[userID]
-    });
-    // playSound(5)
-    updateSettings();
-}
-function check(userID){
-    console.log('in check', browser.management.get(httpsEverywhereID));
-    console.log("fulfill?", MakeQuerablePromise(browser.management.get(httpsEverywhereID)).isFulfilled())
-    console.log("fail?", MakeQuerablePromise(browser.management.get(httpsEverywhereID)).isRejected())
-    console.log("pending?",MakeQuerablePromise(browser.management.get(httpsEverywhereID)).isPending())
-    if (MakeQuerablePromise(browser.management.get(httpsEverywhereID)).isFulfilled()) {//fulfilled
-        console.log( "HTTPS Everywhere has been installed.")
-        browser.alarms.clearAll() //effectively turn off the extension
-        updateSettings()
+// Check if the extension is installed and print out the result right away
+browser.management
+  .get(httpsEverywhereID)
+  .then(result => console.log(result))
+  .catch(err => {
+    if (err.message.includes("No such addon")) {
+      console.warn("(0) extension not installed");
+    } else {
+      console.log(err);
     }
+  });
 
-    else if (MakeQuerablePromise(browser.management.get(httpsEverywhereID)).isPending()) {//rejected or pending = fulfilled is false
-        nudge(userID);
-    }
+/** An API for checking if extension is installed using callbacks */
+function checkIfExtensionInstalled1(extensionID, callback) {
+  browser.management
+    .get(extensionID)
+    .then(() => callback(true))
+    .catch(err => {
+      if (err.message.includes("No such addon")) {
+        callback(false);
+      } else {
+        reject(err);
+      }
+    });
 }
 
-// function playSound (volume) {
-//     if (volume === 0) {
-//         return
-//     }
-//
-//     var sound = new Audio(browser.extension.getURL('sounds/ping.wav'))
-//     sound.volume = volume
-//     sound.play()
-// }
+checkIfExtensionInstalled1(httpsEverywhereID, installed => {
+  console.log(`(1) extension is ${installed ? "" : "NOT "}installed`);
+});
 
-
-function handleInstalled() {
-    Notification.requestPermission().then(function(result) {
-        if (result === 'denied') {
-            console.log('Permission wasn\'t granted. Allow a retry.');
-            return;
+/** An API for checking if extension is installed using an explicitly constructed Promise */
+function checkIfExtensionInstalled2(extensionID) {
+  return new Promise((resolve, reject) => {
+    browser.management
+      .get(extensionID)
+      .then(() => resolve(true))
+      .catch(err => {
+        if (err.message.includes("No such addon")) {
+          resolve(false);
+        } else {
+          reject(err);
         }
-        if (result === 'default') {
-            console.log('The permission request was dismissed.');
-        }
-        // localStorage.setItem('hostID', Client.id); //experimental api
-        localStorage.setItem('hostID', 100);
-        localStorage.setItem('nudgeRepeat', 0);
-        var dates= [Date.now()]
-        console.log(dates)
-        localStorage.setItem('times', dates);
-        localStorage.setItem('period', [15, 60, 480, 1440]);
-        localStorage.setItem('messages', [
-                        //1= no commitment -->  4= soft commitment
-                        'HTTPS-everywhere is ready for installation. Please click to install HTTPS-everywhere.', //toast
-                        'HTTPS-everywhere is ready for installation. When would you be like to be reminded by?', //toast
-                        'HTTPS-everywhere is ready for installation. Please click to install HTTPS-everywhere.', //require interaction
-                        'HTTPS-everywhere is ready for installation. When would you like to enable the browser extension?', //require interaction
-                    ])
-        console.log('checking if user has h.e.');
-        resetAlarm(userID);
-
-        check(userID)
-     });
+      });
+  });
 }
 
-handleInstalled();
+checkIfExtensionInstalled2(httpsEverywhereID).then(installed => {
+  console.log(`(2) extension is ${installed ? "" : "NOT "}installed`);
+});
 
+/** An API for checking if extension is installed using chained Promises */
+function checkIfExtensionInstalled3(extensionID) {
+  return browser.management
+    .get(extensionID)
+    .then(() => true)
+    .catch(err => {
+      if (err.message.includes("No such addon")) {
+        return false;
+      } else {
+        throw err;
+      }
+    });
+}
 
-
-
+checkIfExtensionInstalled3(httpsEverywhereID).then(installed => {
+  console.log(`(3) extension is ${installed ? "" : "NOT "}installed`);
+});
